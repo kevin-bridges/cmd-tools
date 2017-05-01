002ef27e838d01cd853dda3033d30de81b4958b9
Kevin Bridges <kevin.b.bridges@gmail.com>
2017-05-01 16:04:12 -0400

    Update myerror.bat

--- myerror.bat
+++ myerror.bat
@@ -5,8 +5,8 @@ echo Toggle Errorlevel
 if [%1]==[0] goto clear_error
 if [%1]==[1] goto set_error
 echo."Please provide a 1 to throw an error or a 0 to clear the error"
-
 goto end
+
 :clear_error
 exit /b 0
 goto end
