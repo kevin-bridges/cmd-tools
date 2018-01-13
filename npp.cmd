37e6c2df7455d5b79e0c2cf0eff05fa36f459050
Kevin Bridges <kevin.b.bridges@gmail.com>
2018-01-12 20:26:15 -0500

    NPP install location fix

--- npp.cmd
+++ npp.cmd
@@ -1,2 +1,21 @@
 @echo off
-start /b "Notepad++" "C:\Program Files (x86)\Notepad++\notepad++.exe" %*
+setlocal
+
+:: If installed in program files
+if exist "C:\Program Files\Notepad++\notepad++.exe" (
+	start /b "Notepad++" "C:\Program Files\Notepad++\notepad++.exe" %*
+	goto end
+)
+
+:: If installed in program files x86
+if exist "C:\Program Files(86)\Notepad++\notepad++.exe" (
+	start /b "Notepad++" "C:\Program Files(86)\Notepad++\notepad++.exe" %*
+	goto end
+)
+
+:: If installed by Chocolatey
+notepad++ %*
+
+endlocal
+
+:end
