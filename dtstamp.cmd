cc82f006da383bdee3f30c603c3a84eaeb8da83b
Kevin Bridges <kevin.b.bridges@gmail.com>
2018-06-14 14:34:37 -0400

    Update dtstamp.cmd

--- dtstamp.cmd
+++ dtstamp.cmd
@@ -1,5 +1,18 @@
 @echo off
+if [%1]==[/s] set skip=true
+if [%1]==[/silent] set skip=true
+
 
 :: Set's datestamp and timestamp env variables
 set datestamp=%date:~4,2%%date:~7,2%%date:~10,4%
 set timestamp=%time:~0,2%%time:~3,2%%time:~6,2%
+
+if [%skip%]==[true] goto bottom
+echo %%datestamp%% is %datestamp%
+echo %%timestamp%% is %timestamp%
+
+:bottom
+
+set skip=
+
+:EOF
