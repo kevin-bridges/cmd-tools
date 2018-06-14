9d37b364da10cdae03799d9d604a9922fab80e78
Kevin Bridges <kevin.b.bridges@gmail.com>
2018-06-14 14:17:49 -0400

    Delete datestamp.cmd
    created a better file

--- datestamp.cmd
+++
@@ -1,7 +0,0 @@
-@echo on
-
-FOR /F "TOKENS=1* DELIMS= " %%A IN ('DATE/T') DO SET CDATE=%%B
-FOR /F "TOKENS=1,2 eol=/ DELIMS=/ " %%A IN ('DATE/T') DO SET mm=%%B
-FOR /F "TOKENS=1,2 DELIMS=/ eol=/" %%A IN ('echo %CDATE%') DO SET dd=%%B
-FOR /F "TOKENS=2,3 DELIMS=/ " %%A IN ('echo %CDATE%') DO SET yyyy=%%B
-SET date=%mm%%dd%%yyyy%
